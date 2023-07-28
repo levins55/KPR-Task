e6f7ade7bd74f6805bdcc49a40869007a1b09437
levins55 <levinsbscit2019@gmail.com>
2023-07-28 15:45:14 +0530

    change

---
+++ kpr-task/src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made with ♥ by {" "}
+        <a
+          href="https://github.com/levins55/KPR-Task"
+          style={{ cursor: "pointer" }}
+        >
+          LEVIN S
+        </a>
+      </div>
+    );
+  };
+  
+  export default Footer;
+  
